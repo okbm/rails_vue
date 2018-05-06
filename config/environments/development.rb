0044b0540432d5bd73ac94bbeb6e84555b60bfdc
okbm <masayoshiokabe.321@gmail.com>
2018-05-06 14:15:42 +0900

    fix: setup bug fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+# Raise an error on page load if there are pending migrations.  config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
@@ -60,4 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.webpacker.check_yarn_integrity = false
 end
